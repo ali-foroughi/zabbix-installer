c6e293531ac814dd121fbc5717b45647c38c375d
Ali Foroughi <ali.foroughi92@gmail.com>
2021-08-26 11:41:03 +0430

    corrected the pid file for ubuntu
    the pid fire directory on ubuntu was corrected. also added variables for server IPs instead of manually entering them

--- zabbix.sh
+++ zabbix.sh
@@ -1,48 +1,75 @@
 #!/bin/bash
 
+#changed these to server sever IPs
+IR_IP="185.88.179.41"
+DE_IP="138.201.79.7"
+
 read -p 'enter server name (example: vm1122):' NAME
 read -p 'Please specify server location (ir/de):' LOCATION
 OsType=$(cat /etc/os-release | grep NAME | cut -d '"' -f2 | head -n 1 | cut -d ' ' -f1)
 
+
 IR_zabbix_install () {
     		touch /etc/zabbix/zabbix_agentd.conf
     		cat <<EOT >> /etc/zabbix/zabbix_agentd.conf
-    		PidFile=/var/run/zabbix/zabbix_agentd.pid
-    		LogFile=/var/log/zabbix/zabbix_agentd.log
-    		LogFileSize=1
-    		Include=/etc/zabbix/zabbix_agentd.d/*.conf
-    		TLSConnect=psk
-    		TLSAccept=psk
-    		TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
-    		Server=irzbx.rackset.com
-    		ServerActive=irzbx.rackset.com
-    		Hostname=$NAME.euhosted.com
-    		TLSPSKIdentity=PSK $NAME
+LogFile=/var/log/zabbix/zabbix_agentd.log
+LogFileSize=20
+Include=/etc/zabbix/zabbix_agentd.d/*.conf
+TLSConnect=psk
+TLSAccept=psk
+TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
+Server=irzbx.rackset.com
+ServerActive=irzbx.rackset.com
+Hostname=$NAME.euhosted.com
+TLSPSKIdentity=PSK $NAME
 EOT
-    		
+			if [ $OsType == "Ubuntu" ] ; then
+				cat <<EOT >> /etc/zabbix/zabbix_agentd.conf
+PidFile=/run/zabbix/zabbix_agentd.pid
+EOT
+			elif [ $OsType == "CentOs" ] ; then
+				cat <<EOT >> /etc/zabbix/zabbix_agentd.conf
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+EOT
+			else
+				echo "OS tyoe not supported. Please use Ubuntu or CentOS"
+			fi
+
     		# Add the Zabbix server IP to the CSF configuration
-    		echo "tcp|in|d=10050|s=185.88.179.41" >> /etc/csf/csf.allow
-    		echo "tcp|out|d=10051|d=185.88.179.41" >> /etc/csf/csf.allow
+    		echo "tcp|in|d=10050|s=$IR_IP" >> /etc/csf/csf.allow
+    		echo "tcp|out|d=10051|d=$IR_IP" >> /etc/csf/csf.allow
 }
+
 
 DE_zabbix_install () {
     		touch /etc/zabbix/zabbix_agentd.conf
     		cat <<EOT >> /etc/zabbix/zabbix_agentd.conf
-    		PidFile=/var/run/zabbix/zabbix_agentd.pid
-    		LogFile=/var/log/zabbix/zabbix_agentd.log
-	    	LogFileSize=1
-	    	Include=/etc/zabbix/zabbix_agentd.d/*.conf
-	    	TLSConnect=psk
-	    	TLSAccept=psk
-	    	TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
-	    	Server=zabbix.rackset.com
-	    	ServerActive=zabbix.rackset.com
-	    	Hostname=$NAME.euhosted.com
-	    	TLSPSKIdentity=PSK $NAME
+LogFile=/var/log/zabbix/zabbix_agentd.log
+LogFileSize=20
+Include=/etc/zabbix/zabbix_agentd.d/*.conf
+TLSConnect=psk
+TLSAccept=psk
+TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
+Server=zabbix.rackset.com
+ServerActive=zabbix.rackset.com
+Hostname=$NAME.euhosted.com
+TLSPSKIdentity=PSK $NAME
 EOT
+			if [ $OsType == "Ubuntu" ] ; then
+				cat <<EOT >> /etc/zabbix/zabbix_agentd.conf
+PidFile=/run/zabbix/zabbix_agentd.pid
+EOT
+			elif [ $OsType == "CentOs" ] ; then
+				cat <<EOT >> /etc/zabbix/zabbix_agentd.conf
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+EOT
+			else
+				echo "OS tyoe not supported. Please use Ubuntu or CentOS"
+			fi
+
     		# Add the Zabbix server IP to the CSF configuration
-    		echo "tcp|in|d=10050|s=138.201.79.7" >> /etc/csf/csf.allow
-    		echo "tcp|out|d=10051|d=138.201.79.7" >> /etc/csf/csf.allow
+    		echo "tcp|in|d=10050|s=$DE_IP" >> /etc/csf/csf.allow
+    		echo "tcp|out|d=10051|d=$DE_IP" >> /etc/csf/csf.allow
 }
 
 if [ $OsType == "CentOS" ] ; then
@@ -58,6 +85,8 @@ if [ $OsType == "CentOS" ] ; then
 
 	elif [ $LOCATION == "de" ]; then
 		DE_server_install
+	else
+		echo "Location is incorrect. Please choose ir/de"
 	fi	
    		
 elif [ $OsType == "Ubuntu" ] ; then
@@ -71,12 +100,13 @@ elif [ $OsType == "Ubuntu" ] ; then
 	rm /etc/zabbix/zabbix_agentd.conf
 	
 	if [ $LOCATION == "ir" ]; then
-		IR_server_install
+		IR_zabbix_install
 
 
 	elif [ $LOCATION == "de" ]; then
-		DE_server_install
-
+		DE_zabbix_install
+	else
+		echo "Location is incorrect. Please choose ir/de"
 	fi
 else
 	echo "OS type not supported"
@@ -101,5 +131,6 @@ SERVER_HOSTNAME=$(hostname)
 SERVER_IP=$(hostname -i)
 echo "Hostname: $SERVER_HOSTNAME"
 echo "Server IP: $SERVER_IP"
+echo "PSK ID: PSK $NAME"
 echo "Server PSK: $SERVER_PSK"
 echo "###############################"
